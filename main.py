5545df1295055433f631a1fde649f97071e12909
fgclue <schefferdaudtfelipe@gmail.com>
2023-06-23 22:31:19 -0300

    cooooomit

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Kanji")
+root.geometry("500x500")
+
+# Do not add a menu bar. 
+# menubar = Menu(root)
+
+root.config(menu=menubar)
+
+root.mainloop()
